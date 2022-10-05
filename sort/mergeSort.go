6488b2f33c2b5622ae3cbf2f10d8507b1576250f
dsoloview <dsoloview44@gmail.com>
2022-10-05 21:03:25 +0300

    some changes

--- sort/mergeSort.go
+++ sort/mergeSort.go
@@ -1,6 +1,8 @@
 package sort
 
-import "algorithms/types"
+import (
+	"algorithms/types"
+)
 
 func MergeSort[T types.Number](items []T) []T {
 	if len(items) == 1 {
@@ -9,28 +11,29 @@ func MergeSort[T types.Number](items []T) []T {
 
 	first := MergeSort(items[:len(items)/2])
 	second := MergeSort(items[len(items)/2:])
-	return merge(first, second)
 
+	return merge(first, second)
 }
 
-func merge[T types.Number](first []T, seconds []T) []T {
+func merge[T types.Number](first []T, second []T) []T {
 	var result []T
 	var i, j int
-	for i < len(first) && j < len(seconds) {
-		if first[i] < seconds[j] {
+	for i < len(first) && j < len(second) {
+		if first[i] > second[j] {
+			result = append(result, second[j])
+			j++
+		} else {
 			result = append(result, first[i])
 			i++
-		} else {
-			result = append(result, seconds[j])
-			j++
 		}
 	}
 
 	for ; i < len(first); i++ {
 		result = append(result, first[i])
 	}
-	for ; j < len(seconds); j++ {
-		result = append(result, seconds[j])
+
+	for ; j < len(second); j++ {
+		result = append(result, second[j])
 	}
 
 	return result
